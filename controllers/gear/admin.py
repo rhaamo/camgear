78621569e84bacbc20a0213bf6796ff32d423e69
Dashie <dashie@sigpipe.me>
2021-04-26 18:31:51 +0200

    layout

--- controllers/gear/admin.py
+++ controllers/gear/admin.py
@@ -5,6 +5,29 @@ from .models import Camera, Manufacturer, Mount
 
 
 class CameraAdmin(CommonAdmin):
+    list_display = (
+        'id',
+        'camera_model',
+        'state',
+        'mount',
+        'camera_focale',
+        'camera_aperture',
+        'focus',
+    )
+
+    search_fields = ('name', 'state_notes', 'model_notes', 'description')
+
+    def camera_model(self, obj):
+        return obj.__str__()
+    
+    def camera_focale(self, obj):
+        return f"{obj.focale_min or 'x'} - {obj.focale_max or 'x'}"
+    
+    def camera_aperture(self, obj):
+        return f"{obj.min_aperture or 'x'} - {obj.max_aperture or 'x'}"
+
+    list_filter = ('manufacturer', 'mount', 'film_type')
+
     fields = (
         ( 'manufacturer', 'model', 'serial', ),
         ('state', 'state_notes', 'model_notes'),
