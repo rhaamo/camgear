12a2c792d427c9c3f367aa99c57c23caf34e64fd
Dashie <dashie@sigpipe.me>
2024-12-04 23:05:24 +0100

    add middleware

--- camgear/settings.py
+++ camgear/settings.py
@@ -68,6 +68,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
 ]
 
 ROOT_URLCONF = "camgear.urls"
