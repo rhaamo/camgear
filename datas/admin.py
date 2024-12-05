d0841f23a7e30b3e46a6c267fdaeac2c465ea95a
Dashie <dashie@sigpipe.me>
2024-12-05 18:16:23 +0100

    code better uwu

--- datas/admin.py
+++ datas/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from .models import System, Manufacturer
 from camgear.common import CommonAdmin
-from django.contrib.admin.widgets import FilteredSelectMultiple
-from django.db import models
-from django.contrib.admin.widgets import FilteredSelectMultiple
+
 
 class SystemAdmin(CommonAdmin):
     search_fields = ("name",)
+
 
 class ManufacturerAdmin(CommonAdmin):
     search_fields = ("name",)
