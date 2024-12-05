5b3739e750e0e1b91608242df338ca2982bfdad9
Dashie <dashie@sigpipe.me>
2024-12-05 18:15:16 +0100

    use another widget for system manufacturers choice

--- datas/admin.py
+++ datas/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from .models import System, Manufacturer
 from camgear.common import CommonAdmin
-
+from django.contrib.admin.widgets import FilteredSelectMultiple
+from django.db import models
+from django.contrib.admin.widgets import FilteredSelectMultiple
 
 class SystemAdmin(CommonAdmin):
     list_filter = ("manufacturers",)
     search_fields = ("name",)
+
+    formfield_overrides = {
+        models.ManyToManyField: {'widget': FilteredSelectMultiple('Manufacturers', is_stacked=False)}
+    }
 
 
 class ManufacturerAdmin(CommonAdmin):
